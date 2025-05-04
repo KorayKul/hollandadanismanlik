1f4d1850ac4ff6fdd72d3e4def06eb4c9f49126d
Koray Kul <111971623+KorayKul@users.noreply.github.com>
2025-05-05 01:59:50 +0200

    Delete assets directory

--- assets/js/menu.js
+++
@@ -1,9 +0,0 @@
-
-function toggleMenu() {
-  const menu = document.getElementById("mobileMenu");
-  if (menu.style.width === "250px") {
-    menu.style.width = "0";
-  } else {
-    menu.style.width = "250px";
-  }
-}
