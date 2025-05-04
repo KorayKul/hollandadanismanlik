e5cfee06ae15e94ca991f36e1d885f8de60245dd
Koray Kul <111971623+KorayKul@users.noreply.github.com>
2025-05-04 22:48:38 +0200

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,17 @@
+
+// Basit scroll mesajı (örnek)
+window.addEventListener('scroll', () => {
+  console.log('Sayfa kaydırıldı.');
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
+
+  if (toggleButton && navLinks) {
+    toggleButton.addEventListener("click", () => {
+      navLinks.classList.toggle("active");
+    });
+  }
+});
